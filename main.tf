a5d78e7edaecfb4a61b665e0fab5d9fbc5ca6e31
Sasha <36157613+Dorison@users.noreply.github.com>
2022-07-29 11:18:10 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "image_storage" {
+  bucket = "${var.env}-cbb-bucket"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "l1" {
+  bucket = aws_s3_bucket.image_storage.id
+  rule {
+    status = "Enabled"
+    id     = "expire_all_files"
+    noncurrent_version_expiration {
+        noncurrent_days = 5
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "image_storage_acl" {
+  bucket = aws_s3_bucket.image_storage.id
+  acl    = "public-read"
+}
